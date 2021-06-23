b1970b72a39e7a0905c5f8ac2a8dac05a319a1b1
mayu-torii <mayu1152525@yahoo.co.jp>
2021-06-23 14:46:37 +0900

    画像投稿機能実装

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,27 @@
 class RecipesController < ApplicationController
   def index
+    @recipes = Recipe.all
   end
 
   def new
+    @recipe = Recipe.new
   end
   
   def create
-  end  
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :description, :image).merge(user_id: current_user.id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "recipes#index"
-  resources :prototypes, only: [:new, :create, :show, :edit, :update, :destroy] do
+  resources :recipes, only: [:new, :create, :show, :edit, :update, :destroy] do
   end  
 end

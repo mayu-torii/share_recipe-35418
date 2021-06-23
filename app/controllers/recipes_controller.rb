52fd890b3e97e7ed034eeadd98046243953515e9
mayu-torii <mayu1152525@yahoo.co.jp>
2021-06-23 17:54:49 +0900

    編集、削除機能実装

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,9 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.all.includes(:user)
   end
 
   def new
@@ -16,12 +19,40 @@ class RecipesController < ApplicationController
     end
   end
 
-  
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      redirect_to recipe_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @recipe.destroy
+       redirect_to root_path
+    else
+       redirect_to root_path
+    end
+  end  
+
 
   private
 
   def recipe_params
     params.require(:recipe).permit(:title, :description, :image).merge(user_id: current_user.id)
+  end
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
   end
 
 end
